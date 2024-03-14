a91b9ae75067648a87d6c25aae285a6dc3fea51d
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2024-03-14 17:49:31 -0300

    deleted unnecessary fragment

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -3,12 +3,10 @@ import {CartCounter} from '@/app/shopping-cart'
 export default function CounterPage() {
 
   return (
-    <>
     <div className="flex flex-col items-center justify-center w-full h-full">
       <span>Productos en el carro</span> 
       <CartCounter/>
     </div>
-    </>
 
   );
 }
