c0730f85765db2e3ab481b6dd646f246befbf90a
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2024-03-14 13:28:35 -0300

    added shopping cart and divide logic for client side and server side

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,46 +1,12 @@
-'use client'
-import { useState } from "react";
+import {CartCounter} from '@/app/shopping-cart'
 
-type Props = {
-  x:number
-}
-
-export default function CounterPage({x}:Props) {
-
-  const [counter, setCounter] = useState<number>(0);
-
-  const pressBtnPlus = () => {
-    setCounter(counter + 1);
-    setCounter((count) => count + 1)
-  }
-  const pressBtnMinus = () => {
-    setCounter((less) => less - 1)
-
-  }
-  const pressBtnReset =() => setCounter(0);
+export default function CounterPage() {
 
   return (
     <>
     <div className="flex flex-col items-center justify-center w-full h-full">
-      <span>Productos en el carro</span>
-      <span className="text-9xl">{counter}</span>
-
-      <div className="flex">
-        <button 
-        className="flex items-center  justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-        onClick={pressBtnPlus}>
-          +1
-        </button>
-        <button className="flex items-center  justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-        onClick={pressBtnMinus} >
-          -1
-        </button>
-         <button className="flex items-center  justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-        onClick={pressBtnReset}>
-          Reset
-        </button> 
-
-      </div>
+      <span>Productos en el carro</span> 
+      <CartCounter/>
     </div>
     </>
 
---
+++ src/app/shopping-cart/components/CartCounter.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { useState } from "react";
+
+interface Props{
+    value?:number
+}
+
+export function CartCounter({ value =10}:Props) {
+    const [counter, setCounter] = useState(value);
+
+  const pressBtnPlus = () => {
+    setCounter(counter + 1);
+    setCounter((count) => count + 1)
+  }
+  const pressBtnMinus = () => {
+    setCounter((less) => less - 1)
+
+  }
+  const pressBtnReset =() => setCounter(value);
+    return (
+        <>
+            <span className="text-9xl">{counter}</span>
+
+            <div className="flex">
+                <button
+                    className="flex items-center  justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+                    onClick={pressBtnPlus}>
+                    +1
+                </button>
+                <button className="flex items-center  justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+                    onClick={pressBtnMinus} >
+                    -1
+                </button>
+                <button className="flex items-center  justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+                    onClick={pressBtnReset}>
+                    Reset
+                </button>
+
+            </div>
+        </>
+    )
+}
