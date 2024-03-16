391256a4e6456a154c1e6bdb3ac8259b5dc3f8dc
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2024-03-16 17:36:24 -0300

    fixed page

--- src/app/dashboard/movies/page.tsx
+++ src/app/dashboard/movies/page.tsx
@@ -1,7 +1,7 @@
-import { Movie, MovieResponse} from "@/app/movies/interfaces/movies-reponse";
+import { Movie, MovieResponse } from "@/app/movies/interfaces/movies-reponse";
 import Image from 'next/image'
 
-const getMovies = async ():Promise<MovieResponse> => {
+const getMovies = async (): Promise<MovieResponse> => {
   const response = await fetch('https://imdb-top-100-movies.p.rapidapi.com/', {
     headers: {
       'X-RapidAPI-Key': 'ebdcabf4abmsh4836fcef0ccb747p119624jsn93a81e1f6fdc',
@@ -13,26 +13,26 @@ const getMovies = async ():Promise<MovieResponse> => {
   return data;
 }
 
-const getMovie = async (id:string):Promise<Movie> => {
-  const response:Movie = await fetch(`https://imdb-top-100-movies.p.rapidapi.com/top${id}`, {
+const getMovie = async (id: string): Promise<Movie> => {
+  const response: Movie = await fetch(`https://imdb-top-100-movies.p.rapidapi.com/top${id}`, {
     headers: {
       'X-RapidAPI-Key': 'ebdcabf4abmsh4836fcef0ccb747p119624jsn93a81e1f6fdc',
       'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
-    } 
+    }
   })
-  .then(response => response.json())
-  .then(data => {
-     return {
-       rank: data.rank,
-       title: data.title,
-       year: data.year,
-       image: data.image,
-       director: data.director,
-       genre: data.genre,
-       description:data.description
-     };
-  });
- 
+    .then(response => response.json())
+    .then(data => {
+      return {
+        rank: data.rank,
+        title: data.title,
+        year: data.year,
+        image: data.image,
+        director: data.director,
+        genre: data.genre,
+        description: data.description
+      };
+    });
+
   return response;
 
 }
@@ -44,59 +44,28 @@ export default async function MoviesPage() {
   const movie = await getMovie('20');
 
   return (
-    <div style={{ width: '250px' }} className="flex flex-col items-center justify-center">
-    {movies.map((movie, index) => (
-      <div key={index} className="movie-card">
-        <Image
-        key={movie.rank}
-        className="flex items-center"
-          src={movie.image}
-          alt={`Imagen de ${movie.title}`}
-          width={100}
-          height={200}
-        />
-        <span>Ranking: {movie.rank}</span>
-        <span>Titulo: {movie.title}</span>
-        <span>Descripción: {movie.description}</span>
-        <span>Director: {movie.director}</span>
-        <span>Genero: {movie.genre.join(', ')}</span>
+    <div className="flex flex-col">
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+        {movies.map((movie, index) => (
+          <div key={movie.rank} className="flex flex-col items-center w-56">
+            <Image
+              className="flex items-center"
+              src={movie.image}
+              alt={movie.title}
+              width={100}
+              height={100}
+            />
+            <h3>{movie.title}</h3>
+            <p>{movie.rank}</p>
+            <p>{movie.director}</p>
+            <p>{movie.description}</p>
+            <p>{movie.genre}</p>
+            
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
+      
 
-    // <div style={{ width: '500px' }} className="flex flex-col">
-    //   <span>
-    //     {JSON.stringify(movies
-    //     )}
-    //   </span>
-    //   <span>
-    //     <Image
-    //     src={movie.image} 
-    //     alt="Imagen de Movies"
-    //     width={100}
-    //     height={200}
-    //     ></Image>
-    //   </span>
-    //   <span>
-    //     Ranking: {movie.rank}
-    //   </span>
-    //   <span>
-    //     Titulo: {movie.title}
-    //   </span>
-    //   <span>
-    //     Descripción: {movie.description}
-    //   </span>
-    //   <span>
-    //     Director: {movie.director}
-    //   </span>
-    //   <span>
-    //     Genero: {movie.genre}
-    //   </span>
-     
-    // </div>
-
-  
-
-  
+    </div>
   );
 }
